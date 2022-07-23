52172531ff0bc5240a0f95f6e6444f25d6f6014c
Amisha <asin448@aucklanduni.ac.nz>
2022-07-23 13:58:01 +1200

    Implemented bare bones of the monster select screen with some CSS

--- src/components/ChooseMonster/ChooseMonster.js
+++ src/components/ChooseMonster/ChooseMonster.js
@@ -1,8 +1,27 @@
 import React from "react";
+import "./styles.css";
+import monsterOne from "./images/monster1.png";
+import monsterTwo from "./images/monster2.png";
+import monsterThree from "./images/monster3.png";
 
 // hello
 function ChooseMonster() {
-  return <div>ChooseMonster</div>;
+  return (
+    <div className="mainContainer">
+      <button className="left-section">
+        <span className="title">CHOOSE</span>
+        <img className="monster-image" src={monsterOne} alt="" />
+      </button>
+      <button className="middle-section">
+        <span className="title">YOUR</span>
+        <img className="monster-image" src={monsterTwo} alt="" />
+      </button>
+      <button className="right-section">
+        <span className="title">MONSTER</span>
+        <img className="monster-image" src={monsterThree} alt="" />
+      </button>
+    </div>
+  );
 }
 
 export default ChooseMonster;
