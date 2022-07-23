79d2c9f5884017ac551304afb965d76b70a01c01
Amisha <asin448@aucklanduni.ac.nz>
2022-07-23 17:23:14 +1200

    Completed skeleton and main implementation of choose monster screen. Only some style adjustments to be made.

--- src/components/ChooseMonster/ChooseMonster.js
+++ src/components/ChooseMonster/ChooseMonster.js
@@ -9,43 +9,60 @@ import monsterThreeGIF from "./images/monster3.GIF";
 
 function ChooseMonster() {
   return (
-    <div className="mainContainer">
-      <button className="box-section-1">
-        <span className="title">CHOOSE</span>
-        <img className="monster-image" id="monster-1" src={monsterOne} alt="" />
-        <img
-          className="monster-gif"
-          id="monster-1-gif"
-          src={monsterOneGIF}
-          alt=""
-        />
-      </button>
-      <button className="box-section-2">
-        <span className="title">YOUR</span>
-        <img className="monster-image" id="monster-2" src={monsterTwo} alt="" />
-        <img
-          className="monster-gif"
-          id="monster-2-gif"
-          src={monsterTwoGIF}
-          alt=""
-        />
-      </button>
-      <button className="box-section-3">
-        <span className="title">MONSTER</span>
-        <img
-          className="monster-image"
-          id="monster-3"
-          src={monsterThree}
-          alt=""
-        />
-        <img
-          className="monster-gif"
-          id="monster-3-gif"
-          src={monsterThreeGIF}
-          alt=""
-        />
-      </button>
-    </div>
+    <>
+      <h1>TASKFIGHTER</h1>
+
+      <div className="box-container">
+        <button className="box-section-1">
+          <span className="title">CHOOSE</span>
+          <img
+            className="monster-image"
+            id="monster-1"
+            src={monsterOne}
+            alt=""
+          />
+          <img
+            className="monster-gif"
+            id="monster-1-gif"
+            src={monsterOneGIF}
+            alt=""
+          />
+          <span className="name">NAS-TY</span>
+        </button>
+        <button className="box-section-2">
+          <span className="title">YOUR</span>
+          <img
+            className="monster-image"
+            id="monster-2"
+            src={monsterTwo}
+            alt=""
+          />
+          <img
+            className="monster-gif"
+            id="monster-2-gif"
+            src={monsterTwoGIF}
+            alt=""
+          />
+          <span className="name">ASH-LEIGH</span>
+        </button>
+        <button className="box-section-3">
+          <span className="title">MONSTER</span>
+          <img
+            className="monster-image"
+            id="monster-3"
+            src={monsterThree}
+            alt=""
+          />
+          <img
+            className="monster-gif"
+            id="monster-3-gif"
+            src={monsterThreeGIF}
+            alt=""
+          />
+          <span className="name">BON-KCO</span>
+        </button>
+      </div>
+    </>
   );
 }
 
