c80a59be185d3958fd5fefe8d6562d52807222a7
Amisha <asin448@aucklanduni.ac.nz>
2022-07-24 00:03:29 +1200

    Implemented ending page for users to play again and see newly earned badges.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import './App.css';
-import HealthBar from './components/HealthBar/HealthBar';
-import TaskBar from './components/TaskBar/TaskBar';
-import LeafParticles from './components/Particles/LeafParticles';
-import RainParticles from './components/Particles/RainParticles';
-import SmokeParticles from './components/Particles/SmokeParticles';
-import LandingPage from './components/LandingPage/LandingPage';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import "./App.css";
+import HealthBar from "./components/HealthBar/HealthBar";
+import TaskBar from "./components/TaskBar/TaskBar";
+import LeafParticles from "./components/Particles/LeafParticles";
+import RainParticles from "./components/Particles/RainParticles";
+import SmokeParticles from "./components/Particles/SmokeParticles";
+import LandingPage from "./components/LandingPage/LandingPage";
+import EndPage from "./components/EndPage/EndPage";
+//import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
-  
   return (
     <>
-      <LandingPage/>
-      <HealthBar/>
-      <TaskBar/>
+      <LandingPage />
+      <HealthBar />
+      <TaskBar />
+      <EndPage />
     </>
   );
 }
---
+++ src/components/EndPage/EndPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles.css";
+import badgeIcon from "./badge.png";
+
+function EndPage() {
+  return (
+    <>
+      <div className="text-container">
+        <h1 className="header">CONGRATULATIONS, YOU SLAYED!</h1>
+
+        <div className="message-container">
+          <img className="badge-icon" src={badgeIcon}></img>
+          <h2>You've earned a new badge!</h2>
+        </div>
+
+        <button className="replay-button"> PLAY AGAIN?</button>
+      </div>
+    </>
+  );
+}
+
+export default EndPage;
