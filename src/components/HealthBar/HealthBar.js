a829025cfa471f4fd41bc907f0a0ac65259e44bb
lia-arroyo <liayzabel@gmail.com>
2022-07-23 13:22:43 +1200

    health bar decreases and increases using the functions

--- src/components/HealthBar/HealthBar.js
+++ src/components/HealthBar/HealthBar.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState } from 'react';
+import LinearProgress from '@mui/material/LinearProgress';
+import { Button } from '@mui/material';
 
 function HealthBar() {
-  return (
-    <div>HealthBars</div>
+
+    // initiate health levels
+    const [health, setHealth] = useState(100);
+    
+    // decreases health
+    function Attack(){
+        setHealth(health - 10)
+    }
+
+    // the health of the monster regenerates
+    function MonsterRegen(){
+        setHealth(health + 10)
+    }
+
+    return (
+        <>
+            <LinearProgress variant='determinate' value={health} />
+            <Button variant='outlined' onClick={() => Attack()}>ATTACK</Button>
+            <Button variant='outlined' onClick={() => MonsterRegen()}>MONSTER</Button>
+        </>
   )
 }
 
